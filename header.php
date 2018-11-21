166111ea3b7a50578edf92ddcffa9866a9846692
Vini Torino <viniciustorino333@gmail.com>
2018-11-20 23:06:05 -0200

    I created signup.php file

---
+++ header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type-"text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+				    <li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<form action="" method="post">
+						<input type="text" name="uid" placeholder="Username/email">
+						<input type="password" name="password" placeholder="password">
+						 <button type="submit" name="submit">Login</button>
+					</form>
+					<a href="sigup.php">Sign Up</a>
+				</div>
+			</div>
+		</nav>
+	</header>
--- index.php
+++ index.php
@@ -1,32 +1,11 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type-"text/css" href="style.css">
-	<title></title>
-</head>
-<body>
-	<header>
-		<nav>
-			<div class="main-wrapper">
-				<ul>
-				    <li><a href="index.php">Home</a></li>
-				</ul>
-				<div class="nav-login">
-					<form action="" method="post">
-						<input type="text" name="uid" placeholder="Username/email">
-						<input type="password" name="password" placeholder="password">
-						 <button type="submit" name="submit">Login</button>
-					</form>
-					<a href="sigup.php">Sign Up</a>
-				</div>
-			</div>
-		</nav>
-	</header>
+<?php 
+	include_once 'header.php';
+?>
 	<section class="main-container">
-		<div class="main_wrapper">
+		<div class="main-wrapper">
 			<h2>Home</h2>
 		</div>
 	</section>
-</body>
-</html>
+<?php
+	include_once 'footer.php';
+?>
