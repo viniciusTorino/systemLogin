4308744ec850cd546dfe0ea83c3da1f2f1ba2775
Vini Torino <viniciustorino333@gmail.com>
2018-11-21 02:16:53 -0200

    testing it

--- signup.php
+++ signup.php
@@ -4,6 +4,14 @@
 	<section class="main-container">
 		<div class="main-wrapper">
 			<h2>Sign Up</h2>
+			<form class="signup-form" action="" method="post">
+				<input type="text" name="first_name" placeholder="First Name">
+				<input type="text" name="last_name" placeholder="Last Name">
+				<input type="email" name="user_email" placeholder="Email">
+				<input type="text" name="uid" placeholder="Username">
+				<input type="password" name="password" placeholder="Password">
+				<button type="submit" name="submit ">Sign Up</button>
+			</form>  
 		</div>
 	</section>
 <?php
