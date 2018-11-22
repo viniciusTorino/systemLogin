0e4ef985f6089415ccf061f509852ab4e6633dc2
Vini Torino <viniciustorino333@gmail.com>
2018-11-21 23:29:12 -0200

    feeling sleepy

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,4 +1,6 @@
 <?php
+y
+
 //looking for errors first
 if( !(isset($_POST['submit'])) ){
 	header("Location: ../signup.php");
@@ -22,8 +24,8 @@ if( empty($first_name) || empty($last_name) || empty($user_email)
 }
 //checking wheater our character are valid
 // preg_match is some  sort equal find of python 
-if( !(preg_match("/^[a-zA-Z]*$", $first_name ) || 
-    !(preg_match("/^[a-zA-Z]*$", $last_name ) {
+if( !(preg_match("/^[a-zA-Z]*$/", $first_name ) || 
+    !(preg_match("/^[a-zA-Z]*$/", $last_name ) {
 	header("Location: ../signup.php?signup=invalid");
 	exit(); // if i am using exit() Why should I use else ??
 }
@@ -32,8 +34,14 @@ if( !(preg_match("/^[a-zA-Z]*$", $first_name ) ||
 if( !(filter_var($email, FILTER_VALIDATE_EMAIL)) ) { header("Location: ../signup.php?signup=invalidEmail"); exit(); }
 
 //cheacking  if the username has already been taken 
-if( mysqli_num_rows( mysqli_query($connection, "SELECT * FROM users WHERE user_uid = '$user_uid' ") ) > 0) {
+if( mysqli_num_rows( mysqli_query($connection, "SELECT * FROM users WHERE user_uid = '$user_uid' ;") ) > 0) {
 	header("Location: ../signup.php?signup=userTaken"); exit();
 }
 
+// If we were sucessfull in each and every  test we are going to :
+//First >>hasing the password
+$hashed = password_hash($user_password, PASSWORD_DEFALT);
 
+//now we are going to  INSERT INTO our TABLE
+$sql = "INSERT INTO users(first_name, last_name, user_email, user_uid, user_password) VALUES('$first_name', '$last_name', '$user_email', '$user_uid','$hashed');";
+mysqli_query($connection, $sql);
